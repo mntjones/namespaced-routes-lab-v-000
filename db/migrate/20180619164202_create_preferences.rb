de1938e93655f9b80688c12bc42cce1adeffe93a
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-19 16:47:23 +0000

    automatically backed up by learn

--- db/migrate/20180619164202_create_preferences.rb
+++ db/migrate/20180619164202_create_preferences.rb
@@ -3,8 +3,8 @@ class CreatePreferences < ActiveRecord::Migration
     create_table :preferences do |t|
       t.string :song_sort_order
       t.string :artist_sort_order
-      t.boolean :
-allow_create_artists: false, allow_create_songs: true
+      t.boolean :allow_create_artists 
+      t.boolean :allow_create_songs
       t.timestamps null: false
     end
   end
